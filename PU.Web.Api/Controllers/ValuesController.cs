bd050a55d6deb29641838ad039530ad84bde7f43
Paulo <pubach@gmail.com>
2015-08-08 19:04:48 -0400

    Création du projet Web Api

---
+++ PU.Web.Api/Controllers/ValuesController.cs
@@ -0,0 +1,12 @@
+﻿namespace PU.Web.Api.Controllers
+{
+    using System.Web.Http;
+
+    public class ValuesController : ApiController
+    {
+        public IHttpActionResult GetValues()
+        {
+            return Ok(new[] { "a", "b", "c" });
+        }
+    }
+}
